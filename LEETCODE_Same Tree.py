62b05efa0c5194187fe09dca4979c3349b989950
Tahsinul Haque Dhrubo <71776452+thDhrubo@users.noreply.github.com>
2023-01-10 21:34:26 +0600

    Create LEETCODE_Same Tree.py

---
+++ LEETCODE_Same Tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        stack = [(p, q)]
+        while stack:
+            n1, n2 = stack.pop()
+            if n1 is None and n2 is None:
+                continue
+            if n1 is None or n2 is None:
+                return False
+            if n1.val != n2.val:
+                return False
+            stack.append((n1.left, n2.left))
+            stack.append((n1.right, n2.right))
+        return True
+            
