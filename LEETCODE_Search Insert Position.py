6389ed5eafcaa11a239c63e6aa339e61a37c77e3
Tahsinul Haque Dhrubo <71776452+thDhrubo@users.noreply.github.com>
2023-01-18 00:33:45 +0600

    Create LEETCODE_Search Insert Position.py

---
+++ LEETCODE_Search Insert Position.py
@@ -0,0 +1,13 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left+right)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left+=1
+            else:
+                right-=1
+        return left
