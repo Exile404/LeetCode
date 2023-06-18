66ee43d433dd71d73276b181662d10b6509eec18
Tahsinul Haque Dhrubo <71776452+Exile404@users.noreply.github.com>
2023-06-18 23:57:53 +0600

    v0.

---
+++ LEETCODE_Count and Say.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        if n == 1:
+            return "1"
+        else:
+            prev = self.countAndSay(n - 1)
+            result = ""
+            i = 0
+            while i < len(prev):
+                count = 1
+                while i + 1 < len(prev) and prev[i] == prev[i+1]:
+                    i += 1
+                    count += 1
+                result += str(count) + prev[i]
+                i += 1
+            return result
