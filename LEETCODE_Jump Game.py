250106e76e54f2684f2615073aa66723f8898a76
Tahsinul Haque Dhrubo <71776452+Exile404@users.noreply.github.com>
2023-06-19 00:58:28 +0600

    v0.0.1

--- LEETCODE_Jump Game.py
+++ LEETCODE_Jump Game.py
@@ -1,25 +1,11 @@
 class Solution:
-    def trap(self, height: List[int]) -> int:
-        n = len(height)
-        if n == 0:
-            return 0
-
-        left, right = 0, n-1
-        maxLeft, maxRight = 0, 0
-        water = [0] * n
-
-        while left <= right:
-            if height[left] <= height[right]:
-                if height[left] > maxLeft:
-                    maxLeft = height[left]
-                else:
-                    water[left] = maxLeft - height[left]
-                left += 1
-            else:
-                if height[right] > maxRight:
-                    maxRight = height[right]
-                else:
-                    water[right] = maxRight - height[right]
-                right -= 1
-
-        return sum(water)
+    def canJump(self, nums: List[int]) -> bool:
+        max_index = 0
+        n = len(nums)
+        for i in range(n):
+            if i > max_index:
+                return False
+            max_index = max(max_index, i + nums[i])
+            if max_index >= n - 1:
+                return True
+        return False
