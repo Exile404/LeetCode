541b830e4eb3cfe6fcde7f42584093c9d02f9758
Tahsinul Haque Dhrubo <71776452+Exile404@users.noreply.github.com>
2023-06-28 03:36:25 +0600

    v0.0.1

---
+++ H-Index.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort(reverse=True)
+        h = 0
+        for i in range(len(citations)):
+            if citations[i] >= i + 1:
+                h = i + 1
+            else:
+                break
+
+        return h
+        
