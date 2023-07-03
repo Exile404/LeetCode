c8303faddfbac2e4322ae5550ea8e1c9f7085228
Tahsinul Haque Dhrubo <71776452+Exile404@users.noreply.github.com>
2023-07-03 23:22:28 +0600

    v0.0.1

---
+++ LEETCODE_Summary Ranges.py
@@ -0,0 +1,25 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        if len(nums) == 0:
+            return []
+        ranges = []
+        start = nums[0]
+        end = nums[0]
+
+        for i in range(1, len(nums)):
+            if nums[i] == end + 1:
+                end = nums[i]
+            else:
+                if start == end:
+                    ranges.append(str(start))
+                else:
+                    ranges.append(str(start) + '->' + str(end))
+                start = nums[i]
+                end = nums[i]
+
+        if start == end:
+            ranges.append(str(start))
+        else:
+            ranges.append(str(start) + '->' + str(end))
+
+        return ranges
